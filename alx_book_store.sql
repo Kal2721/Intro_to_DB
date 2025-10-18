16532e048b8ceeaf8d8d8ddb8b62b85cb894548e
kalkidan <kalkidanteshale6@gmail.com>
2025-10-18 14:57:42 +0300

    designing a MySQL database for an online bookstore

---
+++ alx_book_store.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL,
+);
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    published_date DATE,
+    price DOUBLE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    address TEXT NOT NULL
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
+
+SHOW TABLES;
+DESCRIBE Authors;
+DESCRIBE Books;
+DESCRIBE Customers;
+DESCRIBE Orders;
+DESCRIBE Order_Details;
