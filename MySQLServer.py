04d4c35087b3adab97394c20872a4a53b7982af6
kalkidan <kalkidanteshale6@gmail.com>
2025-10-18 15:19:06 +0300

    Creating the database alx_book_store in your MySQL server

--- MySQLServer.py
+++ MySQLServer.py
@@ -11,7 +11,7 @@ def create_database():
 
         if connection.is_connected():
             cursor = connection.cursor()
-            cursor.execute("CREATE DATABASE IF NOT EXISTS my_database")
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully.")
     except Error as e:
         print(f"Error while connecting to MySQL: {e}")
